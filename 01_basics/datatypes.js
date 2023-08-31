8b15d47762915cd62da6200f6f0bb840f7535bc1
vinaytheprogrammer <49182318+vinaytheprogrammer@users.noreply.github.com>
2023-08-31 11:27:31 +0000

    Basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,2 @@
+"use strict"; // treat all js code as newer version
+//alert(1+2)   // we are Using Nodejs not Browsers
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+
+const CollegeName="NitjSr"; //  Used for Contant VAlues which will never be changed
+let studentName= "Vinay Gupta"; // mostly prefered to use because it's lifetime only inside of a block
+var CourseName= "MCA";                   // Not Used in Recent Time because of its global scope
+
+
+Interest="Spirituality";  // the way of declaring variable in this way not suitable to code
+
+let a;
+var b;
+const c="1"; // it can't be declared, must need to initialize
+console.log(a)
+console.log(b)
+console.log(c)
+
